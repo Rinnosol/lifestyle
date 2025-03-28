56199fd73456457e0b2f1de74fca0551b910b10d
Rinnosol <139254996+Rinnosol@users.noreply.github.com>
2025-03-28 22:26:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def get_db_connection():
     return pymysql.connect(
         host="localhost",
         user="root",
-        password="admin",
+        password="root",
         database="lifestyle"
     )
 
