fae9f322c98aa35a64fe6facd438be757119ca44
Rinnosol <139254996+Rinnosol@users.noreply.github.com>
2025-03-29 10:18:58 +0530

    Update lifestyle.sql

--- lifestyle.sql
+++ lifestyle.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `bestworkouts`
 --
-
+USE lifestyle;
 DROP TABLE IF EXISTS `bestworkouts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
